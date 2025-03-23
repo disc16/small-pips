2d46afe3d635ced8b94be30a46a244e5396f3c69
Irwen Recorba <irwen.recorba@neoday.com>
2025-03-23 17:57:17 +0800

    icon for timepicker

--- resources/js/Pages/TradeRecords/AddEntry.vue
+++ resources/js/Pages/TradeRecords/AddEntry.vue
@@ -666,20 +666,48 @@ watch(
                                 <InputLabel for="entry_date" value="Entry Date + Time" />
 
                                 <div class="relative max-w-sm">
-                                    <input 
+                                    <div class="relative max-w-sm">
+                                        <!-- <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                                            </svg>
+                                        </div> -->
+                                        <input 
+                                            type="date" 
+                                            id="entry_date" 
+                                            v-model="form.entry_date" 
+                                            :class="`rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 mt-1 block w-full mb-2 ${!enableField ? 'bg-gray-200' : ''}`"
+                                            :disabled="!enableField" 
+                                        />
+                                    </div>
+                                    <!-- <input 
                                         type="date" 
                                         id="entry_date" 
                                         v-model="form.entry_date" 
                                         :class="`rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 mt-1 block w-full mb-2 ${!enableField ? 'bg-gray-200' : ''}`"
                                         :disabled="!enableField" 
-                                    />
-                                    <input 
+                                    /> -->
+                                    <div class="relative">
+                                        <div class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
+                                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                                <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clip-rule="evenodd"/>
+                                            </svg>
+                                        </div>
+                                        <input 
+                                            type="time" 
+                                            id="entry_time" 
+                                            v-model="form.entry_time" 
+                                            :class="`rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 mt-1 block w-full  ${!enableField ? 'bg-gray-200' : ''}`" 
+                                            :disabled="!enableField"
+                                        />
+                                    </div>
+                                    <!-- <input 
                                         type="time" 
-                                        id="entry_date" 
+                                        id="entry_time" 
                                         v-model="form.entry_time" 
                                         :class="`rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 mt-1 block w-full  ${!enableField ? 'bg-gray-200' : ''}`" 
                                         :disabled="!enableField"
-                                    />
+                                    /> -->
                                 </div>
 
                                 <InputError class="mt-2" :message="form.errors.first_name" />
